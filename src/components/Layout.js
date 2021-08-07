91f0afd81918494a89b047b2381d1680a1db35b9
Kevin Schueller <kevinschueller@gmail.com>
2021-08-07 00:24:56 -0400

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -17,6 +17,7 @@ export default class Body extends React.Component {
                     <meta name="google" content="notranslate" />
                     <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,700,700i" rel="stylesheet"/>
                     <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
+                    <meta name="p:domain_verify" content="9ba7ba83b4d41cb546f1610403c13df9"/>
                 </Helmet>
                 <div id="page" className={'site palette-' + _.get(this.props, 'pageContext.site.siteMetadata.color_scheme') + ' accent-' + _.get(this.props, 'pageContext.site.siteMetadata.accent_color')}>
                   <Header {...this.props} />
